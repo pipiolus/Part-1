d85246ddc69b017059ff870da8c3539425e6cd69
pipiolus <giovanipaolonii@gmail.com>
2023-09-06 16:24:50 -0300

    Step1 done. Initialized app

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,7 +1,31 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
 function App() {
-  return <></>;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleBad = () => setBad(bad + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
+
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
 }
 
 export default App;
